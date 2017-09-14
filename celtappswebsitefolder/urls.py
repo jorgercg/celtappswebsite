d5acc6067850dd2f3c06addff9a356ffca4a300b
Jorge Goncalves <jrcgonc@gmail.com>
2017-09-14 14:54:34 +0100

    Celtapps Website - 14SEP2017 14:54

--- celtappswebsitefolder/urls.py
+++ celtappswebsitefolder/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from celtappswebsiteapp import celtapps
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', celtapps.index, name='index'),
 ]
